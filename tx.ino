8daec9eeb2b0439e597b935ddd7ff90e235f5b98
Abraham Rodriguez <38869805+Arendost@users.noreply.github.com>
2021-02-04 13:24:58 -0400

    Create tx.ino

---
+++ tx.ino
@@ -0,0 +1,13 @@
+void tx(){   
+ Serial.print(h1);
+ Serial.print(h2);
+ Serial.print(':');
+ Serial.print(m1);
+ Serial.print(m2);
+ Serial.print(':');
+ Serial.print(s1);
+ Serial.print(s2);
+ Serial.println(t); 
+ delay(1000);
+  
+}
